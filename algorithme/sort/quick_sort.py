5cddcb0981f51150705eed055eda611b9b8d1515
홍동수 <sug5806@naver.com>
2021-05-11 20:51:43 +0900

    quick sort 수정

--- algorithme/sort/quick_sort.py
+++ algorithme/sort/quick_sort.py
@@ -8,18 +8,15 @@ def quick_sort(list_data):
     pivot = list_data[len(list_data) // 2]
 
     left = [item for item in list_data if pivot > item]
-    print(left)
 
     right = [item for item in list_data if pivot < item]
-    print(right)
-    print()
 
     return quick_sort(left) + [pivot] + quick_sort(right)
 
 
 if __name__ == '__main__':
-    data = random.sample(range(10), 5)
+    data = random.sample(range(1000), 30)
 
-    print(data)
+    print("정렬 전 : ", data)
     print()
-    print(quick_sort(data))
+    print("정렬 후 : ", quick_sort(data))
