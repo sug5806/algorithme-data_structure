539e2d9892af7b9b59c2ce232773aa6a30788a4b
홍동수 <sug5806@naver.com>
2021-05-14 19:50:40 +0900

    이진 트리 및 이진 탐색 트리 작성

---
+++ data_structure/tree/node_management.py
@@ -0,0 +1,36 @@
+import node
+
+
+class NodeManagement:
+    def __init__(self, root):
+        self.current_node = None
+        self.root = root
+
+    def insert(self, value):
+        self.current_node = self.root
+        while True:
+            if value < self.current_node.value:
+                if self.current_node.left is not None:
+                    self.current_node = self.current_node.left
+                else:
+                    self.current_node.left = node.Node(value)
+                    break
+            else:
+                if self.current_node.right is not None:
+                    self.current_node = self.current_node.right
+                else:
+                    self.current_node.right = node.Node(value)
+                    break
+
+    def search(self, value):
+        self.current_node = self.root
+
+        while self.current_node:
+            if self.current_node.value == value:
+                return True
+            elif value < self.current_node.value:
+                self.current_node = self.current_node.left
+            else:
+                self.current_node = self.current_node.right
+
+        return False
