57e968203d9d2ded6cb141be6eacc53c3c5ce2ba
홍동수 <sug5806@naver.com>
2021-05-18 16:52:57 +0900

    백준 알고리즘 문제 1966번 풀이

---
+++ problem_solving/1966_프린터_큐.py
@@ -0,0 +1,19 @@
+if __name__ == "__main__":
+    test_case = int(input())
+
+    for _ in range(test_case):
+        n, m = list(map(int, input().split(' ')))
+        queue = list(map(int, input().split(' ')))
+        queue = [(i, idx) for idx, i in enumerate(queue)]
+
+        count = 0
+        while True:
+            if queue[0][0] == max(queue, key=lambda x: x[0])[0]:
+                count += 1
+                if m == queue[0][1]:
+                    print(count)
+                    break
+                else:
+                    queue.pop(0)
+            else:
+                queue.append(queue.pop(0))
