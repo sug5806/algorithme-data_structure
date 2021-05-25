88e3f6f668fab28b10568b44e4cd1eab39d9e065
홍동수 <sug5806@naver.com>
2021-05-25 13:56:22 +0900

    1번 문제 풀이 추가 5번 문제 풀이 추가

---
+++ problem_solving/leet_code/sequence_data_structure/1_two_sum.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+def twoSum(nums: List[int], target: int) -> List[int]:
+    nums_map = {}
+
+    # 키와 값을 바꿔서 저장
+    for i, num in enumerate(nums):
+        nums_map[num] = i
+
+    print(nums_map)
+
+    # 타겟에서 첫 번째 수를 뺀 결과를 키로 조회
+    for i, num in enumerate(nums):
+        if target - num in nums_map and i != nums_map[target - num]:
+            return [i, nums_map[target - num]]
+
+
+if __name__ == "__main__":
+    nums = [2, 7, 11, 15]
+    target = 13
+
+    print(twoSum(nums, target))
