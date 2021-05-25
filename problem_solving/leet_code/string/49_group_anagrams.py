bbbfd4ad0b11720238a14c156eecb453e2977396
홍동수 <sug5806@naver.com>
2021-05-25 11:45:56 +0900

    49번 문제 풀이

---
+++ problem_solving/leet_code/string/49_group_anagrams.py
@@ -0,0 +1,16 @@
+import collections
+from typing import List
+
+
+def groupAnagrams(strs: List[str]) -> List[List[str]]:
+    anagram = collections.defaultdict(list)
+
+    for word in strs:
+        anagram[''.join(sorted(word))].append(word)
+
+    return list(anagram.values())
+
+
+if __name__ == "__main__":
+    example = ["eat", "tea", "tan", "ate", "nat", "bat"]
+    groupAnagrams(example)
