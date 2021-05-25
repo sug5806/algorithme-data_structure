77c49bbe4f5d00b7014fc86029980d5ccae8a5e8
홍동수 <sug5806@naver.com>
2021-05-25 11:35:02 +0900

    819번 문제풀이 추가

---
+++ problem_solving/leet_code/string/819_most_common_word.py
@@ -0,0 +1,19 @@
+import collections
+import re
+from typing import List
+
+
+def mostCommonWord(paragraph: str, banned: List[str]) -> str:
+    words = [word for word in re.sub(r'[^\w]', ' ', paragraph)
+        .lower().split()
+             if word not in banned]
+
+    counts = collections.Counter(words)
+    return counts.most_common(1)[0][0]
+
+
+if __name__ == "__main__":
+    example = "Bob hit a ball, the hit BALL flew far after it was hit."
+    ban_char = ["hit"]
+
+    mostCommonWord(example, ban_char)
